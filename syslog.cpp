d7241bd4434b2556dac81d5eadd99aa3ac29bdd1
Zack <zackbpro@gmail.com>
2018-01-19 20:16:09 -0700

    log creation is functional

--- syslog.cpp
+++ syslog.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 static int callback(void* data, int argc, char** argv, char** azColName){
 	int i=0;
-	printf("Inn callback\n");
+	printf("In callback\n");
 	for(int i=0;i<argc;i++){
 		printf("%d %s",i, argv[i]);
 	}
 	return 0;
 }
 
+/*This function prints the usage information of the program*/
 void print_usage(const char* bin_name){
 	printf("USAGE: %s SET [log type]\n", bin_name);
 	printf("       %s REC <number of logs>\n", bin_name);
@@ -57,6 +58,18 @@ int verify_args(int argc, char const* argv[]){
 	}
 	return method;
 }
+/**
+* This function converts a string to its uppercase equivalent
+*/
+char* to_upper(const char* arg){
+	int size = strlen(arg);
+	char* upper_str = new char[size];
+	for(int i=0;i<size;i++){
+		upper_str[i] = toupper(arg[i]);
+	}
+	printf("New str is %s\n", upper_str);
+	return upper_str;
+}
 int main(int argc, char const *argv[])
 {
 	verify_args(argc, argv);
@@ -68,6 +81,28 @@ int main(int argc, char const *argv[])
 	}
 	const char* create_table_sql = "CREATE TABLE if not exists logs(ID INTEGER PRIMARY KEY AUTOINCREMENT, LOG_TIME DATETIME NOT NULL, LOG_TYPE NVARCHAR NOT NULL, COMMENTS VARCHAR)";
 	rc = sqlite3_exec(db, create_table_sql, callback, 0, 0);
+	/* SET action */
+	if(strcmp(argv[1],"SET")==0){
+		char* log_type = to_upper(argv[2]);
+		printf("Log Type: %s\n", log_type);
+		printf("Additional comment - ");
+		char* comment = new char[1024];
+		scanf("%s",comment);
+		time_t cur_time = time(NULL);
+		struct tm tm = *localtime(&cur_time);
+		stringstream insert_sql_ss;
+		insert_sql_ss << "INSERT INTO logs(LOG_TIME, LOG_TYPE, COMMENTS) VALUES (\"" << tm.tm_year+1900 << "-" << tm.tm_mon+1 << "-" << tm.tm_mday << " " << tm.tm_hour << ":" << tm.tm_min << ":" << tm.tm_sec << "\",\"" << log_type << "\",\"" << comment << "\");";
+		string insert_sql_str = insert_sql_ss.str();
+		printf("SQL statement: %s",insert_sql_str.c_str());
+		const char* insert_sql_cstr = insert_sql_str.c_str();
+		rc = sqlite3_exec(db, insert_sql_cstr, callback,0,0);
+		if(rc==SQLITE_ABORT){
+			fprintf(stderr, "[ERROR] Error inserting into db, exiting\n");
+			exit(_DB_QUERY_ERROR);
+		}
+
+	}
+	/*REC action */
 
 	return 0;
 }
--- syslog.h
+++ syslog.h
@@ -2,7 +2,18 @@
 #include <cstdlib>
 #include <sqlite3.h>
 #include <string.h>
+#include <ctype.h>
+#include <iostream>
+#include <time.h>
+#include <sstream>
+
 const int _DB_CONN_ERROR = -1;
 const int _BAD_ARGS = -2;
 const int _HELP_MSG = -3;
+const int _DB_QUERY_ERROR = -4;
+
+
 static int callback(void*, int, char**, char**);
+void print_usage(const char*);
+int verify_args(int, const char*);
+char* to_upper(char*);
