0d860897b0f46b3b6c0b251e9f8786711c151ed3
Zack <zack.baker@protonmail.ch>
2018-07-20 23:22:08 -0600

    started writing the stages of the actual client as they are executed

--- src/sisyl_client.c
+++ src/sisyl_client.c
@@ -13,8 +13,30 @@
 */
 int main(int argc, char** argv)
 {
-	Params* p = process_args(argc, argv);
+	Params* p = (Params*)(process_args(argc, argv));
 	printf("return_code: %d\n", p->return_code);
+	if(p->return_code!=0){//if params are bad
+		return 1;//return non-zero
+	}
+
+	//Do set stuff here
+	char* user = (getpwuid(getuid()))->pw_name;//get the username of the user who invoked the client
+	time_t secs = time(NULL);//get the current UNIX time
+	char secs_str[64];
+	sprintf(secs_str,"%ld", secs);//convert the time into a character array for the query (TODO: Figure out proper size)
+
+	QueryParams* qp = malloc(sizeof(QueryParams));
+	qp->title = p->title;
+	qp->description = p->description;
+	qp->user=user;
+	qp->timestr=secs_str;
+	qp->level=p->level;
+	free(p);
+	char* query = get_daemon_insert_query(qp);
+	free(qp);
+
+
+	//Do get stuff here, once params struct is fleshed out
 
 	return 0;
 }
