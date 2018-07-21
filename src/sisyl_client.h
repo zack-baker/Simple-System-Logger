1379733c14bc3ce1a8eda644f441b981b6d9dee1
Zack <zack.baker@protonmail.ch>
2018-07-20 23:21:31 -0600

    Created QueryParams struct to pass to get_daemon_insert_query

--- src/sisyl_client.h
+++ src/sisyl_client.h
@@ -13,16 +13,25 @@
 typedef struct{
 	char* title;
 	char* description;
-	int level;
+	char* level;//might make char* so we dont need to convert unneccesarily from char* to int, then back from int to char* when creating the daemon inserter struct
 	char* request_type;
 
 	int return_code;
 } Params;
 
+typedef struct{
+	char* title;
+	char* description;
+	char* level;
+	char* user;
+	char* timestr;
+
+} QueryParams;	
+
 void print_usage(int, char*);
 char* send_request(char*);
-void query_daemon_insert(char*, char*, char*, char*, char*);
+char* get_daemon_insert_query(QueryParams*);
 char* get_description(int,char**);
-Params* process_args(int, char**);
+void* process_args(int, char**);
 void print_args(int, char**);
 
